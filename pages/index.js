96eeb900ee0539c33d028d0801810e85c87511b7
bySharkster <fernandoaponte0609@gmail.com>
2023-01-21 23:10:19 -0400

    Pages running

--- pages/index.js
+++ pages/index.js
@@ -15,17 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico"/>
       </Head>
       <main>
-      <div className="hero min-h-screen" style={{ backgroundImage: `url("/IMG/bg.jpg")` }}>
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-           <Image src='/IMG/promo-vida.jpeg' 
-                  alt='promo-vida'
-                  width={500}
-                  height={500}/>
-          </div>
-        </div>
-      </div>
+     
       </main>
     </>
   )
--- pages/services.js
+++ pages/services.js
@@ -2,7 +2,7 @@ import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa'
 import Image from 'next/Image'
 
 
-export default function About() {
+export default function Services() {
     return (
      <div></div>
     )
